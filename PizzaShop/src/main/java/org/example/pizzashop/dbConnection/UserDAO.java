78309b5d9c6b5397137e2a305a15ec7e6d44f12e
G.Dávid <davidmaestro444@gmail.com>
2025-04-25 13:34:28 +0200

    User és UserDAO létrehozása
    User: kezeli a usereket
    UserDAO: felhasználó hozzáadása,létezőfelhasználó ellenőrzése

---
+++ PizzaShop/src/main/java/org/example/pizzashop/dbConnection/UserDAO.java
@@ -0,0 +1,53 @@
+package org.example.pizzashop.dbConnection;
+import org.example.pizzashop.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+    private final Connection connection;
+
+    public UserDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean registerUser(User user) {
+        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getEmail());
+            int rowsInserted = statement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User loginUser(String username, String password) {
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return new User(
+                        resultSet.getInt("id"),
+                        resultSet.getString("username"),
+                        resultSet.getString("password"),
+                        resultSet.getString("email")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
