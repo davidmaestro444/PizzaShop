971631ab34251bd022b94d69d48510b6031d37a3
prokajistvan03 <114095564+prokajistvan03@users.noreply.github.com>
2025-05-13 14:32:04 +0200

    unit teszt
    még két unit teszt meg lett írva

--- PizzaShop/src/test/java/TestCases.java
+++ PizzaShop/src/test/java/TestCases.java
@@ -1,10 +1,19 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import org.example.pizzashop.controllers.CartController;
 import org.example.pizzashop.dbConnection.DatabaseConnection;
+import org.example.pizzashop.model.*;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TestCases {
     @Test
@@ -24,5 +33,33 @@ public class TestCases {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    void testGetAllAvailableToppings() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pizzaShop", "root", "");
+
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute("SELECT * FROM toppings");
+        }
+
+        PizzaRepository dao = new PizzaRepository();
+        List<Topping> result = dao.getAllAvailableToppings();
+
+        assertEquals(15, result.size());
+    }
+
+    @Test
+    void testGetTotalPrice() {
+        BasicPizza pizza = new BasicPizza(1,"Margarita","", 1000);
+        CartItem cartItem = new CartItem(pizza);
+
+        cartItem.addToppings(List.of(
+                new Topping(1,"Sajt", 200, "", false, false),
+                new Topping(1,"Szalámi", 300, "", true, false)
+        ));
+
+        int actual = cartItem.getTotalPrice();
+        assertEquals(1500, actual);
     }
 }
